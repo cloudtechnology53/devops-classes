e2e19bfda4f2b42971c9789415ebc132d00c5367
cloudtechnology53 <125374902+cloudtechnology53@users.noreply.github.com>
2023-02-15 19:13:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,39 @@
-variable "aws_region" {
-  description = "Provide AWS region"
-  type        = string
-  default     = "ap-south-1"
+# VPC Variables
+variable "region" {
+  default       = "ap-south-1"
+  description   = "AWS Region"
+  type          = string
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.30.0.0/16"
+variable "vpc-cidr" {
+  default       = "10.0.0.0/16"
+  description   = "VPC CIDR Block"
+  type          = string
 }
 
+variable "public-subnet-1-cidr" {
+  default       = "10.0.0.0/24"
+  description   = "Public Subnet 1 CIDR Block"
+  type          = string
+}
 
+variable "public-subnet-2-cidr" {
+  default       = "10.0.1.0/24"
+  description   = "Public Subnet 2 CIDR Block"
+  type          = string
+}
 
+/*
+variable "private-subnet-1-cidr" {
+  default       = "10.0.2.0/24"
+  description   = "Private Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-2-cidr" {
+  default       = "10.0.3.0/24"
+  description   = "Private Subnet 2 CIDR Block"
+  type          = string
+}
+
+*/
